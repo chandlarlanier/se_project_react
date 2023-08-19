a4840ea921f456c8b47c6ff4062a484a8ed777c7
Chandlar Lanier <cklanier@outlook.com>
2023-08-19 18:34:10 -0400

    App

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import 
+
+function App() {
+  return (
+    <div>
+      <header className='header'>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
