a9bc5d3eabc592f9bc98938c95acd49556c519a0
Chandlar Lanier <cklanier@outlook.com>
2023-08-22 00:39:48 -0400

    Card section

---
+++ src/ItemCard/ItemCard.js
@@ -0,0 +1,14 @@
+import './ItemCard.css';
+
+const ItemCard = ({ item }) => {
+    return (
+        <div>
+            <div>
+                <img src={item.link} className='card__image' alt={item.name}/>
+            </div>
+            <div className='card__name'>{item.name}</div>
+        </div>
+    )
+};
+
+export default ItemCard;
