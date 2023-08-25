96f0a71c9a0f63f3582e0416fe479a7559606460
Chandlar Lanier <cklanier@outlook.com>
2023-08-25 02:28:09 -0400

    Item preview modal css

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -1,11 +1,15 @@
+import './ItemModal.css';
+
 const ItemModal = ({ selectedCard, onClose }) => {
     return (
         <div className='modal'>
             <div className='modal__content'>
-                <button type='button' onClick={onClose}>Close</button>
-                <img src={selectedCard.link} />
-                <div>{selectedCard.name}</div>
-                <div>Weather: {selectedCard.weather}</div>
+                <button type='button' onClick={onClose} class='modal__close-button'>
+                    <img src={require('../../images/close-icon.svg').default} />
+                </button>
+                <img src={selectedCard.link} className='modal__img' />
+                <div className='modal__item-name'>{selectedCard.name}</div>
+                <div className='modal__item-weather'>Weather: {selectedCard.weather}</div>
             </div>
         </div>
     )
