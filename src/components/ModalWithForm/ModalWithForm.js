a3e1f9c4b80d459d9abfdd2ef821486fb52007c2
Chandlar Lanier <cklanier@outlook.com>
2023-08-26 19:27:50 -0400

    Move submit button into form element

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,25 +1,33 @@
-import './ModalWithForm.css';
-import grayCloseIcon from '../../images/close-icon-gray.svg'
+import "./ModalWithForm.css";
+import grayCloseIcon from "../../images/close-icon-gray.svg";
 
 const ModalWithForm = ({
-    children,
-    buttonText = 'Add garment',
-    title,
-    onClose,
-    name
+  children,
+  buttonText = "Add garment",
+  title,
+  onClose,
+  name,
 }) => {
-    return (
-        <div className={`modal modal_type_${name}`}>
-            <div className='modal__content'>
-                <button className='modal__close-button_gray' type='button' onClick={onClose}>
-                    <img src={grayCloseIcon} />
-                </button>
-                <h3 className='modal__title'>{title}</h3>
-                <form className='modal__form'>{children}</form>
-                <button className='modal__submit-button' type='submit'>{buttonText}</button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={`modal modal_type_${name}`}>
+      <div className="modal__content">
+        <button
+          className="modal__close-button_gray"
+          type="button"
+          onClick={onClose}
+        >
+          <img src={grayCloseIcon} alt="Close icon" />
+        </button>
+        <h3 className="modal__title">{title}</h3>
+        <form className="modal__form">
+          {children}
+          <button className="modal__submit-button" type="submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default ModalWithForm;
