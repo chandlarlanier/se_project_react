da791df55affa5a573863d3cd457ded9ac0bc431
Chandlar Lanier <cklanier@outlook.com>
2023-08-26 19:47:39 -0400

    Move weatherOptions variable from WeatherCard.js to constants.js and export/import instead

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,20 +1,5 @@
 import './WeatherCard.css';
-
-const weatherOptions = [
-    { url: require('../../images/day/day-clear.svg').default, day: true, type: 'sunny' },
-    { url: require('../../images/day/day-cloudy.svg').default, day: true, type: 'cloudy' },
-    { url: require('../../images/day/day-fog.svg').default, day: true, type: 'fog' },
-    { url: require('../../images/day/day-rain.svg').default, day: true, type: 'rain' },
-    { url: require('../../images/day/day-snow.svg').default, day: true, type: 'snow' },
-    { url: require('../../images/day/day-storm.svg').default, day: true, type: 'storm' },
-    { url: require('../../images/night/night-clear.svg').default, day: false, type: 'sunny' },
-    { url: require('../../images/night/night-cloudy.svg').default, day: false, type: 'cloudy' },
-    { url: require('../../images/night/night-fog.svg').default, day: false, type: 'fog' },
-    { url: require('../../images/night/night-rain.svg').default, day: false, type: 'rain' },
-    { url: require('../../images/night/night-snow.svg').default, day: false, type: 'snow' },
-    { url: require('../../images/night/night-storm.svg').default, day: false, type: 'storm' }
-    
-]
+import { weatherOptions } from '../../utils/constants';
 
 const WeatherCard = ({ day, type, weatherTemp = 0 }) => {
     const imageSrc = weatherOptions.filter((i) => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,17 @@
+import clearDay from '../images/day/day-clear.svg';
+import cloudyDay from '../images/day/day-cloudy.svg';
+import foggyDay from '../images/day/day-fog.svg';
+import rainyDay from '../images/day/day-rain.svg';
+import snowyDay from '../images/day/day-snow.svg';
+import stormyDay from '../images/day/day-storm.svg';
+import clearNight from '../images/night/night-clear.svg';
+import cloudyNight from '../images/night/night-cloudy.svg';
+import foggyNight from '../images/night/night-fog.svg';
+import rainyNight from '../images/night/night-rain.svg';
+import snowyNight from '../images/night/night-snow.svg';
+import stormyNight from '../images/night/night-storm.svg';
+
+
 const defaultClothingItems = [
   {
     _id: 0,
@@ -37,4 +51,21 @@ const defaultClothingItems = [
   }
 ];
 
-export {defaultClothingItems};
+
+const weatherOptions = [
+  { url: clearDay, day: true, type: 'sunny' },
+  { url: cloudyDay, day: true, type: 'cloudy' },
+  { url: foggyDay, day: true, type: 'fog' },
+  { url: rainyDay, day: true, type: 'rain' },
+  { url: snowyDay, day: true, type: 'snow' },
+  { url: stormyDay, day: true, type: 'storm' },
+  { url: clearNight, day: false, type: 'sunny' },
+  { url: cloudyNight, day: false, type: 'cloudy' },
+  { url: foggyNight, day: false, type: 'fog' },
+  { url: rainyNight, day: false, type: 'rain' },
+  { url: snowyNight, day: false, type: 'snow' },
+  { url: stormyNight, day: false, type: 'storm' }
+  
+]
+
+export {defaultClothingItems, weatherOptions};
