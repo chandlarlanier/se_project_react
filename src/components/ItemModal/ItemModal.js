ed763f8078d590d0cd80d48750e6090a65f3920d
Chandlar Lanier <cklanier@outlook.com>
2023-08-26 19:23:29 -0400

    Import close icon into ItemModal.js and update src

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -1,18 +1,25 @@
-import './ItemModal.css';
+import "./ItemModal.css";
+import closeIcon from "../../images/close-icon.svg";
 
 const ItemModal = ({ selectedCard, onClose }) => {
-    return (
-        <div className='modal'>
-            <div className='modal__content'>
-                <button type='button' onClick={onClose} class='modal__close-button'>
-                    <img src={require('../../images/close-icon.svg').default} />
-                </button>
-                <img src={selectedCard.link} className='modal__img' />
-                <div className='modal__item-name'>{selectedCard.name}</div>
-                <div className='modal__item-weather'>Weather: {selectedCard.weather}</div>
-            </div>
+  return (
+    <div className="modal">
+      <div className="modal__content">
+        <button type="button" onClick={onClose} class="modal__close-button">
+          <img src={closeIcon} alt="Close icon" />
+        </button>
+        <img
+          src={selectedCard.link}
+          className="modal__img"
+          alt="Clothing item"
+        />
+        <p className="modal__item-name">{selectedCard.name}</p>
+        <div className="modal__item-weather">
+          Weather: {selectedCard.weather}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ItemModal;
