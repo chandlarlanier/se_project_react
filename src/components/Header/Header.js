c26a769c7bf242a6f8bd6d098a530d5dc1110235
Chandlar Lanier <cklanier@outlook.com>
2023-08-26 18:39:12 -0400

    Import logo and avatar images into Header.js and update each src

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,28 @@
-import './Header.css';
+import "./Header.css";
+import logo from "../../images/logo.svg";
+import avatar from "../../images/avatar.svg";
 
-const currentDate = new Date().toLocaleString('default', { month: 'long', day: 'numeric' });
+const currentDate = new Date().toLocaleString("default", {
+  month: "long",
+  day: "numeric",
+});
 
 const Header = ({ onCreateModal }) => {
-    return (
-        <header className='header'>
-        <div className='header__left'>
-          <img className='header__logo' src={require('../../images/logo.svg').default}/>
-          <div className='header__date'>{currentDate}, New York</div>
-        </div>
-        <div className='header__right'>
-          <button className='header__button' type='text' onClick={onCreateModal}>+ Add clothes</button>
-          <div className='header__name'>Terrence Tegegne</div>
-          <img className='header__avatar' src={require('../../images/avatar.svg').default} />
-        </div>
-      </header>
-    )
-}
+  return (
+    <header className="header">
+      <div className="header__left">
+        <img className="header__logo" src={logo} alt="WTWR logo" />
+        <div className="header__date">{currentDate}, New York</div>
+      </div>
+      <div className="header__right">
+        <button className="header__button" type="text" onClick={onCreateModal}>
+          + Add clothes
+        </button>
+        <div className="header__name">Terrence Tegegne</div>
+        <img className="header__avatar" src={avatar} alt="User avatar" />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
-   
