f2ebad1bf10e8a4174f698c6b05517ff502a7390
Chandlar Lanier <cklanier@outlook.com>
2023-11-12 23:44:59 -0500

    Add signup and signin logic

---
+++ src/utils/auth.js
@@ -0,0 +1,35 @@
+const baseUrl = "http://localhost:3001";
+
+const signUp = (newUserData) => {
+  const { name, avatar, email, password } = newUserData;
+
+  return fetch(`${baseUrl}/signup`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, avatar, email, password }),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  });
+};
+
+const signIn = (userData) => {
+  const { email, password } = userData;
+
+  return requestAnimationFrame(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  });
+};
